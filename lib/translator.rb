0cc1a019baa6b6e31ea4a8fd481d5f1182d1654c
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-09 11:59:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,14 +3,20 @@ require "yaml"
 
 def load_library(file_path)
   file = YAML.load_file(file_path)
+  #loads the emoticon file 
     emoticons = {}
+    #returns a hash to hold emoticons in 
   file.each do |key, emoji|
+    #iterates through all the emoticons 
     emoticons[key] = {}
-    
+    #points each key to an inner hash
     emoticons[key][:english] = emoji[0]
+    #creates english symbol that points to english emoji
     emoticons[key][:japanese] = emoji[1]
+    #creates japanese symbol that points to japanese emoji
   end 
   emoticons
+  #returns the array
 end
 
 def get_japanese_emoticon(file_path, emoticon)
